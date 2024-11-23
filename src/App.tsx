db837af3e912276e9a18ca2b2c8220a14ad46452
Kyle Murphy <10656368+Kmurphy24@users.noreply.github.com>
2024-11-23 02:44:38 -0500

    user can deal cards. clear cards. show cards and user's current score. shuffles the deck 10 times

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { getFullDeck } from "./utils/cardDeck";
+import { Card, Pip } from "./types/card";
+
+interface UserScore {
+  score: number;
+  secondaryScore: number;
+}
 
 function App() {
+  const cardDeck = useRef<Card[]>(getFullDeck());
   const [chips, setChips] = useState<number>(0);
+  const [dealtCards, setDealtCards] = useState<Card[]>([]);
+  const [userScore, setUserScore] = useState<UserScore>({
+    score: 0,
+    secondaryScore: 0
+  });
 
   useEffect(() => {
     const userChips = localStorage.getItem("userChips");
@@ -14,23 +27,90 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    let currentScore = 0;
+    let currenSecondaryScore = 0;
+    let hasAce = false;
+    dealtCards.forEach((card) => {
+      if (card.pip === Pip.Ace) {
+        currentScore += 1;
+        hasAce = true;
+      } else if (
+        card.pip === Pip.Jack ||
+        card.pip === Pip.Queen ||
+        card.pip === Pip.King
+      ) {
+        currentScore += 10;
+      } else {
+        currentScore += Number(card.pip);
+      }
+    });
+
+    if (hasAce && currentScore < 11) {
+      currenSecondaryScore = currentScore + 11;
+    }
+
+    setUserScore({ score: currentScore, secondaryScore: currenSecondaryScore });
+  }, [dealtCards]);
+
   const setStartingChips = () => {
     setChips(500);
     localStorage.setItem("userChips", "500");
   };
 
-  const bankUserChips = () => {
-    localStorage.setItem("userChips", chips.toString());
+  const dealTwoCards = () => {
+    const card = cardDeck.current.splice(0, 1);
+    setDealtCards(dealtCards.concat(card));
   };
+
+  const resetDeck = () => {
+    setDealtCards([]);
+    cardDeck.current = getFullDeck();
+    shuffleDeck();
+  };
+
+  const shuffleDeck = () => {
+    let shuffleCount = 0;
+    let currentIndex = 51;
+    do {
+      const randomIndex = Math.floor(Math.random() * 51);
+      [cardDeck.current[currentIndex], cardDeck.current[randomIndex]] = [
+        cardDeck.current[randomIndex],
+        cardDeck.current[currentIndex]
+      ];
+      if (currentIndex === 0) {
+        currentIndex = 51;
+        shuffleCount++;
+      } else {
+        currentIndex--;
+      }
+    } while (shuffleCount < 10);
+  };
+
   return (
     <>
-      <div>The user has {chips} chips</div>
-      <button onClick={() => setChips(chips + 1)}>Add a chip</button>
-      <button onClick={() => setChips(chips - 1)}>Remove a chip</button>
-      <button onClick={bankUserChips}>Bank chips</button>
-      <button onClick={() => localStorage.removeItem("userChips")}>
-        Remove local storage chips
+      <div className="p-4">The user has {chips} chips</div>
+      {userScore.score > 21 && <div>Player Bust</div>}
+      <button
+        onClick={dealTwoCards}
+        disabled={userScore.score >= 21}
+        className="disabled:opacity-15"
+      >
+        Deal
       </button>
+      <button onClick={resetDeck}>Clear Cards</button>
+      <div className="flex">
+        {dealtCards.map((card) => (
+          <div key={`${card.pip}-${card.suit}`} className="flex flex-col p-2">
+            <span>{card.pip}</span>
+            <span>{card.suit}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        {userScore.score}
+        {userScore.secondaryScore > 0 && ` / ${userScore.secondaryScore}`}
+      </div>
     </>
   );
 }
